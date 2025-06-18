697f64e31fd708a353925ac6ab1e8b7e1c16f5ee
Andreas Prasetya <andreasprasetya20@gmail.com>
2025-06-18 22:25:06 +0700

    first commit

--- src/main/java/com/juaracoding/tugastasqa/App.java
+++ src/main/java/com/juaracoding/tugastasqa/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
     }
+
+    public static void main1( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+
 }
